6b2b224fc65dad1cdc997c87d102cde475f23cb1
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-15 22:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	var s = "";
+	for(var i = 0;i<text.length;i++) {
+		if(text.charAt(i)!=' ') {
+			s += text.charAt(i);
+			break;
+		}
+	}
+	return s;
 }
 
 // Do not change the code below
